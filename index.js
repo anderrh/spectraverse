03aadd21f71c9d475efd8ae5294790a204860fd7
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2024-01-02 20:22:11 -0800

    Got motion working

--- index.js
+++ index.js
@@ -13,35 +13,98 @@ image.src = 'img/Pokemon Style Game Map.png';
 const playerImage = new Image()
 playerImage.src = './img/playerDown.png';
 
-let backgroundImageX = -925;
+class Sprite {
+  constructor({ position, velocity, image}) {
+    this.position = position
+    this.image = image
+  }
+  draw() {
+    context.drawImage(this.image, this.position.x, this.position.y);
+  }
+}
 
-let backgroundImageY = -500;
+const background = new Sprite({
+  position: {
+    x: -925, y: -500
+  },
+  image: image
+})
+
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
 
 function animate() {
   window.requestAnimationFrame(animate);
-  context.drawImage(image, backgroundImageX, backgroundImageY);
+  background.draw()
   context.drawImage(playerImage, 0, 0, playerImage.width/4, playerImage.height, canvas.width / 2 - playerImage.width / 2 / 4, canvas.height / 2 - playerImage.height / 2, playerImage.width/4, playerImage.height );
+
+  if (keys.w.pressed) {
+    background.position.y = background.position.y + 5
+  }
+  if (keys.a.pressed) {
+    background.position.x = background.position.x + 5
+  }
+  if (keys.s.pressed) {
+    background.position.y = background.position.y - 5
+  }
+  if (keys.d.pressed) {
+    background.position.x = background.position.x - 5
+  }
 }
+
 animate();
-// get correct change x, y
 window.addEventListener('keydown', (e) => {
   switch (e.key) {
     case 'w':
-      backgroundImageY = backgroundImageY + 1
+      keys.w.pressed = true
       console.log('pressed w key');
       break;
     case 'a':
-      backgroundImageX = backgroundImageX + 1
+      keys.a.pressed = true
       console.log('pressed a key');
       break;
     case 's':
-      backgroundImageY = backgroundImageY - 1
+      keys.s.pressed = true
       console.log('pressed s key');
       break;
     case 'd':
-      backgroundImageX = backgroundImageX - 1
+      keys.d.pressed = true
       console.log('pressed d key');
       break;
   }
+  console.log(keys)
 });
 
+window.addEventListener('keyup', (e) => {
+  switch (e.key) {
+    case 'w':
+      keys.w.pressed = false
+      console.log('pressed w key');
+      break;
+    case 'a':
+      keys.a.pressed = false
+      console.log('pressed a key');
+      break;
+    case 's':
+      keys.s.pressed = false
+      console.log('pressed s key');
+      break;
+    case 'd':
+      keys.d.pressed = false
+      console.log('pressed d key');
+      break;
+  }
+  console.log(keys)
+});
