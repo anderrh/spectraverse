6281a48810a7564c531ef0d2c5ff9401e567bf6b
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2024-01-13 21:40:22 -0800

    Collisions are drawn to the screen but do not move with player yet

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
+// We decided to pull out the collisions from the js object here so they are available below.
+function findCollisionLayer(map) {
+  const layers = map["layers"];
+  for (let i = 0; i < layers.length; i+=1) {
+    if (layers[i]["name"] == "Collisions") {
+      return layers[i];
+    } 
+  }
+  return null;
+}
+const collisionsObject = findCollisionLayer(pokemonStyleGameMap)
+const collisions = collisionsObject["data"];
+const levelWidth = collisionsObject["width"];
+const levelHight = collisionsObject["height"]
+const zoomLevel = 4;
+const tileWidth = pokemonStyleGameMap["tilewidth"] * zoomLevel;
+const tileHeight = pokemonStyleGameMap["tileheight"] * zoomLevel;
+// done with prelude
+
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
 canvas.width = 1024;
 canvas.height = 576;
-context.fillStyle = 'white';
-context.fillRect(0, 0, canvas.width,canvas.height);
 
+let collisionsMap = []; 
+for (let i = 0; i < collisions.length; i+=levelWidth) {
+  collisionsMap.push(collisions.slice(i,levelWidth + i))
+}  
+const offset = {
+  x: -925, y: -500
+};
+
+class Boundary {
+  constructor({position}) {
+    this.position = {
+      x: position.x * tileWidth + offset.x,
+      y: position.y * tileHeight + offset.y
+    };
+    this.width = tileWidth;
+    this.height = tileHeight;
+  }
+  draw() {
+    context.fillStyle = '#ff0000'
+    context.fillRect(this.position.x,this.position.y,this.width,this.height)
+  }
+}
+
+let boundaries = [];
+//todo make boundaries move when you move
+for (let i = 0; i < collisions.length; i+=1) {
+  if (collisions[i] === 1025) {
+    boundaries.push(new Boundary(
+      {
+        position:{
+          x: i % levelWidth ,
+          y: Math.floor(i/levelWidth)
+        }
+      }))
+  }
+}
 
 const image = new Image();
 image.src = 'img/Pokemon Style Game Map.png';
@@ -22,10 +75,9 @@ class Sprite {
     context.drawImage(this.image, this.position.x, this.position.y);
   }
 }
-
 const background = new Sprite({
   position: {
-    x: -925, y: -500
+    x: offset.x, y: offset.y
   },
   image: image
 })
@@ -48,6 +100,7 @@ const keys = {
 function animate() {
   window.requestAnimationFrame(animate);
   background.draw()
+  boundaries.forEach(boundary=>boundary.draw())
   context.drawImage(playerImage, 0, 0, playerImage.width/4, playerImage.height, canvas.width / 2 - playerImage.width / 2 / 4, canvas.height / 2 - playerImage.height / 2, playerImage.width/4, playerImage.height );
 
   if (keys.w.pressed) {
